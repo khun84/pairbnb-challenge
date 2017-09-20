21860ddcb44b7403488c6349749e23da7a1a74c5
khun84 <khun84@gmail.com>
2017-09-20 15:43:11 +0800

    added verified column to listing

---
+++ db/migrate/20170920074214_add_verified_column_to_listing.rb
@@ -0,0 +1,5 @@
+class AddVerifiedColumnToListing < ActiveRecord::Migration[5.1]
+  def change
+      add_column :listings, :verified, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920043038) do
+ActiveRecord::Schema.define(version: 20170920074214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20170920043038) do
     t.bigint "user_id"
     t.string "pet"
     t.bigint "location_id"
+    t.boolean "verified"
     t.index ["location_id"], name: "index_listings_on_location_id"
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
