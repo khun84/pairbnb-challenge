e2c671981d3908d05ae3d3d1f7069e659c2b9c48
khun84 <khun84@gmail.com>
2017-09-20 12:39:41 +0800

    added enum role to user in migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170919034438) do
+ActiveRecord::Schema.define(version: 20170920043038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20170919034438) do
     t.string "name", null: false
     t.string "country"
     t.string "state"
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email"
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,47 +14,56 @@
 
 require 'csv'
 ########### LOAD THE LOCATION DATA
-user_ids = User.all.pluck(:id)
-location_path = File.join(File.dirname(__FILE__), 'location.csv')
-csv = CSV.new(File.open(location_path), headers:true)
-Location.transaction do
-    csv.each do |row|
-        location = Location.new
-        location.country = row["Country"]
-        location.state = row["State"]
-        location.city = row["City"]
-        location.save
-    end
-end
+# user_ids = User.all.pluck(:id)
+# location_path = File.join(File.dirname(__FILE__), 'location.csv')
+# csv = CSV.new(File.open(location_path), headers:true)
+# Location.transaction do
+#     csv.each do |row|
+#         location = Location.new
+#         location.country = row["Country"]
+#         location.state = row["State"]
+#         location.city = row["City"]
+#         location.save
+#     end
+# end
 
 ############# LOAD THE LISTING DATA
-location_ids = Location.all.pluck(:id)
-
-listing_path = File.join(File.dirname(__FILE__), 'listing.csv')
-csv = CSV.new(File.open(listing_path), headers:true)
-Listing.transaction do
-    csv.each do |row|
-        listing = Listing.new
-        data = row.to_h
-        listing.user_id = user_ids.sample
-        listing.name = data["property_name"]
-        listing.room_count = data["number_of_rooms"]
-        listing.location_id = location_ids.sample
-        # listing.country = data["country"]
-        # listing.state = data["state"]
-        # listing.city = data["city"]
-        listing.person_count = data["number_of_persons"]
-        listing.base_price = data["base_price"]
-        if data['smoking'] == '1'
-            listing.smoke = 'Y'
+# location_ids = Location.all.pluck(:id)
+#
+# listing_path = File.join(File.dirname(__FILE__), 'listing.csv')
+# csv = CSV.new(File.open(listing_path), headers:true)
+# Listing.transaction do
+#     csv.each do |row|
+#         listing = Listing.new
+#         data = row.to_h
+#         listing.user_id = user_ids.sample
+#         listing.name = data["property_name"]
+#         listing.room_count = data["number_of_rooms"]
+#         listing.location_id = location_ids.sample
+#         # listing.country = data["country"]
+#         # listing.state = data["state"]
+#         # listing.city = data["city"]
+#         listing.person_count = data["number_of_persons"]
+#         listing.base_price = data["base_price"]
+#         if data['smoking'] == '1'
+#             listing.smoke = 'Y'
+#         else
+#             listing.smoke = 'N'
+#         end
+#         if data['pet'] == '1'
+#             listing.pet = "Y"
+#         else
+#             listing.pet = "N"
+#         end
+#         listing.save
+#     end
+# end
+User.transaction do
+    User.all.each do |user|
+        if user.email == 'khun84@gmail.com'
+            user.moderator!
         else
-            listing.smoke = 'N'
+            user.customer!
         end
-        if data['pet'] == '1'
-            listing.pet = "Y"
-        else
-            listing.pet = "N"
-        end
-        listing.save
     end
 end
