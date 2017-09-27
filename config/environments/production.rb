eb1141520e207264df375b1c81389bd666122233
khun84 <khun84@gmail.com>
2017-09-27 11:22:07 +0800

    changed seed file for production testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,9 @@ Rails.application.configure do
           password: ENV["GMAIL_PASSWORD"]
   }
 
+  # configure mailer emails in dev and testing env
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,73 +8,73 @@
 
 
 ########### LOAD THE USER DATA
-# (1..10).each do |i|
-#     User.create(name: "test#{i}", email: "test#{i}@gmail.com", password: 'password')
-# end
+(1..10).each do |i|
+    User.create(name: "test#{i}", email: "test#{i}@gmail.com", password: 'password')
+end
 
 require 'csv'
 ########### LOAD THE LOCATION DATA
-# user_ids = User.all.pluck(:id)
-# location_path = File.join(File.dirname(__FILE__), 'location.csv')
-# csv = CSV.new(File.open(location_path), headers:true)
-# Location.transaction do
-#     csv.each do |row|
-#         location = Location.new
-#         location.country = row["Country"]
-#         location.state = row["State"]
-#         location.city = row["City"]
-#         location.save
-#     end
-# end
+user_ids = User.all.pluck(:id)
+location_path = File.join(File.dirname(__FILE__), 'location.csv')
+csv = CSV.new(File.open(location_path), headers:true)
+Location.transaction do
+    csv.each do |row|
+        location = Location.new
+        location.country = row["Country"]
+        location.state = row["State"]
+        location.city = row["City"]
+        location.save
+    end
+end
 
 ############# LOAD THE LISTING DATA
-# location_ids = Location.all.pluck(:id)
-#
-# listing_path = File.join(File.dirname(__FILE__), 'listing.csv')
-# csv = CSV.new(File.open(listing_path), headers:true)
-# Listing.transaction do
-#     csv.each do |row|
-#         listing = Listing.new
-#         data = row.to_h
-#         listing.user_id = user_ids.sample
-#         listing.name = data["property_name"]
-#         listing.room_count = data["number_of_rooms"]
-#         listing.location_id = location_ids.sample
-#         # listing.country = data["country"]
-#         # listing.state = data["state"]
-#         # listing.city = data["city"]
-#         listing.person_count = data["number_of_persons"]
-#         listing.base_price = data["base_price"]
-#         if data['smoking'] == '1'
-#             listing.smoke = 'Y'
-#         else
-#             listing.smoke = 'N'
-#         end
-#         if data['pet'] == '1'
-#             listing.pet = "Y"
-#         else
-#             listing.pet = "N"
-#         end
-#         listing.save
-#     end
-# end
+location_ids = Location.all.pluck(:id)
+
+listing_path = File.join(File.dirname(__FILE__), 'listing.csv')
+csv = CSV.new(File.open(listing_path), headers:true)
+Listing.transaction do
+    csv.each do |row|
+        listing = Listing.new
+        data = row.to_h
+        listing.user_id = user_ids.sample
+        listing.name = data["property_name"]
+        listing.room_count = data["number_of_rooms"]
+        listing.location_id = location_ids.sample
+        # listing.country = data["country"]
+        # listing.state = data["state"]
+        # listing.city = data["city"]
+        listing.person_count = data["number_of_persons"]
+        listing.base_price = data["base_price"]
+        if data['smoking'] == '1'
+            listing.smoke = 'Y'
+        else
+            listing.smoke = 'N'
+        end
+        if data['pet'] == '1'
+            listing.pet = "Y"
+        else
+            listing.pet = "N"
+        end
+        listing.save
+    end
+end
 
 ############## SET USER ROLE
-# User.transaction do
-#     User.all.each do |user|
-#         if user.email == 'khun84@gmail.com'
-#             user.moderator!
-#         else
-#             user.customer!
-#         end
-#     end
-# end
+User.transaction do
+    User.all.each do |user|
+        if user.email == 'khun84@gmail.com'
+            user.moderator!
+        else
+            user.customer!
+        end
+    end
+end
 
 ############# SET LISTING VERIFIED VALUE
-# Listing.transaction do
-#     Listing.all.each do |listing|
-#         listing.verified = false
-#         listing.save!
-#     end
-# end
+Listing.transaction do
+    Listing.all.each do |listing|
+        listing.verified = false
+        listing.save!
+    end
+end
 
