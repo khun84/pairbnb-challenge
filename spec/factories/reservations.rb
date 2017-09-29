cd44b77cb6540c0bd1afbe96d3c2470ffbc587de
khun84 <khun84@gmail.com>
2017-09-29 16:20:38 +0800

    started user model test

--- spec/factories/reservations.rb
+++ spec/factories/reservations.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
-  factory :reservation do
-    
-  end
+    factory :reservation do
+        check_in Date.new(2017,11,1)
+        check_out Date.new(2017,11,3)
+        num_of_guests 6
+    end
 end
+
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model, focus: true do
+  # pending "add some examples to (or delete) #{__FILE__}"
+    describe 'create new user' do
+        describe 'normal sign up' do
+            context 'successful signup' do
+                it 'should add the user to the database' do
+                    user = create(:user)
+                    user1 = User.find_by(email: user.email)
+                    expect(user1.email).to eq user.email
+                end
+            end
+        end
+
+    end
+end
